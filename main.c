870c1bda8cd63dd576fea604e39b32d76f4c6df5
rena0204 <rena0204@sookmyung.ac.kr>
2025-11-09 21:45:38 +0900

    10 (6)

--- main.c
+++ main.c
@@ -4,18 +4,27 @@
 int main(int argc, char *argv[])
 {
 	char input[100];
-	FILE* fp;
-	int i;
+	FILE*fp;
 	
-	fp=fopen("sample.txt", "w");
-	
-	for(i-0;i<3;i++)
+	fp=fopen ("sample.txt", "r");
+	if(fp==NULL)
 	{
-		printf("input a word : ");
-		scanf("%s", input);
-		fprintf(fp, "%s\n", input);
+		printf("Failed to open file!\n");
+		return -1;
 	}
-	
+	//fprintf
+	#if 0
+	while( (c=fgetc(fp)) != EOF)
+	{
+		putchar(c);
+	}
+	#else 
+	while (fgets(input, 100, fp) > 0)
+	{
+		printf("%s", input);
+	}
+	#endif
+//fclose		
 	fclose(fp);
 	return 0;
 }
